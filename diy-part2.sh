a44e58ff89e1a48928d3c08a85f7b4f2e22e9938
1758976954 <89236575+1758976954@users.noreply.github.com>
2021-12-20 15:47:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -86,6 +86,9 @@ git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
 #### 添加锐捷校园插件
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/lean/luci-app-mentohust
 
+#### 添加闪讯插件
+git clone -b LUCI-LUA-UCITRACK https://github.com/CCnut/feed-netkeeper.git package/lean/feed-netkeeper
+
 #### 添加阿里云盘
 #git clone https://github.com/messense/aliyundrive-webdav.git package/lean/aliyundrive-webdav
 
